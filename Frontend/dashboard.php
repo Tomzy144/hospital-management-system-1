121170acafe7c90b4e3c15710a81097bb253914e
Tomzy.cs,Vb <oyemadetomiwa@gmail.com>
2023-10-16 10:39:42 +0100

    "updated by tomiwa on '16-10-2023'"

--- Frontend/dashboard.php
+++ Frontend/dashboard.php
@@ -1,9 +1,31 @@
-<?php include '../../backend/config/connection.php';?>
-<?php include 'backend/config/session-validation.php';?>
+<?php include '../backend/config/connection.php';?>
+<?php include '../backend/dashboardconfig/session-validation.php';?>
 
 <?php
  $staff_id= $_POST['staff_id'];
 ?>
+
+<?php    
+    $fetch_staff_profile=$callclass->_get_user_details($conn, $s_staff_id);
+    $staff_profile_array = json_decode($fetch_staff_profile, true);
+    $fullname= $staff_profile_array[0]['fullname'];
+    $email= $staff_profile_array[0]['email'];
+    $phonenumber= $staff_profile_array[0]['phonenumber'];
+   // $role_id= $staff_profile_array[0]['role_id'];
+    $status_id= $staff_profile_array[0]['status_id'];
+    $date= $staff_profile_array[0]['date'];
+    $last_login= $staff_profile_array[0]['last_login'];
+   $passport = $staff_profile_array[0]["passport"];
+    
+        
+    $fetch_status=$callclass->_get_status_details($conn, $status_id);
+    $status_array = json_decode($fetch_status, true);
+    $status_name= $status_array[0]['status_name'];
+   
+?>
+ 
+
+
 
 <!DOCTYPE html>
 <html lang="en">
@@ -25,9 +47,12 @@
     <div class="div-logo">
         <a href="#">Hospital management System</a>
     </div>
-    <a href="../index.php">
-    <button class="signIn">SignOut</button>
-</a>
+    
+        <form method="post" action="../backend/dashboardconfig/code.php" id="logoutform">
+            <input type="hidden" name="action" value="logout"/>    
+          
+            <button class="signIn"  onclick="document.getElementById('logoutform').submit();">SignOut</button>
+        </form>
 </div>
 
 <nav class="sidebar" id="sidebar" >
