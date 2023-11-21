8aabc58021235d73bcd30bb4747c7873dc8ff2d0
Tomzy.cs,Vb <oyemadetomiwa@gmail.com>
2023-11-21 16:00:53 +0100

    "updated on 21-Nov-23"

--- js/scripts.js
+++ js/scripts.js
@@ -393,7 +393,7 @@ function _add_patient(){
                     // _get_page('active-staff','active-staff');
                     // alert_close();
                     window.alert("Registration Successful");
-                    window.alert("This patient's ID is" + fpatient_id );
+                    window.alert("This patient's ID is "+ fpatient_id );
                 }
                 $('#proceed-btn').html('<i class="bi-check2"></i> SUBMIT');
                 document.getElementById('proceed-btn').disabled=false;
