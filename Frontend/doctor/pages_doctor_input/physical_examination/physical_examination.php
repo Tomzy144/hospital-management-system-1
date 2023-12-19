afe3b8f14faa8a1bb8b7b84d38149bc35245b5f3
Kingsley Effiong <kingsleyeffiong642@gmail.com>
2023-12-19 17:27:41 +0100

    updated

--- Frontend/doctor/pages_doctor_input/physical_examination/physical_examination.php
+++ Frontend/doctor/pages_doctor_input/physical_examination/physical_examination.php
@@ -86,43 +86,43 @@
             <label for="headache">Eye opening response</label>
                 <div class="radio">
                 <input type="radio" id="spontaneous" name="eye"/>
-                <label for="spontaneous">Spontaneous 4</label>
+                <label for="spontaneous">Spontaneous</label>
                 <input type="radio" id="to_speech" name="eye"/>
-                <label for="to_speech">To speech 3</label>
+                <label for="to_speech">To speech </label>
                 <input type="radio" id="to_pain" name="eye"/>
-                <label for="no_response">To Pain 2</label>
+                <label for="no_response">To Pain </label>
                 <input type="radio" id="no_response" name="eye"/>
-                <label for="no_response">No Response 1</label>
+                <label for="no_response">No Response</label>
                 </div>
 </div>
             <div class="form-control">
             <label for="headache">Best verbal response</label>
                 <div class="radio">
                 <input type="radio" id="oriented" name="verbal"/>
-                <label for="oriented">Oriented to time, place & person 5</label>
+                <label for="oriented">Oriented to time, place & person </label>
                 <input type="radio" id="confused" name="verbal"/>
                 <label for="confused">Confused 3</label>
                 <input type="radio" id="inappropraite" name="verbal"/>
-                <label for="no_response">Inappropraite words 2</label>
+                <label for="no_response">Inappropraite words</label>
                 <input type="radio" id="incmprehensible" name="verbal"/>
-                <label for="response">Incomprehensible sounds 1</label>
+                <label for="response">Incomprehensible sounds</label>
                 <input type="radio" id="response" name="verbal"/>
-                <label for="response">No Response 1</label>
+                <label for="response">No Response</label>
                 </div>
 </div>
             <div class="form-control">
             <label for="headache">Best motor response</label>
                 <div class="radio">
                 <input type="radio" id="obey" name="motor"/>
-                <label for="obey">Obey command 6</label>
+                <label for="obey">Obey command</label>
                 <input type="radio" id="localized" name="motor"/>
-                <label for="localized">Moved to localised pain 5</label>
+                <label for="localized">Moved to localised pain</label>
                 <input type="radio" id="flexion" name="motor"/>
-                <label for="flexion">Flexion withdrawal from pain 3</label>
+                <label for="flexion">Flexion withdrawal from pain</label>
                 <input type="radio" id="abnornal" name="motor"/>
-                <label for="abnormal">Abnormal flexion(decorticate) 2</label>
+                <label for="abnormal">Abnormal flexion(decorticate)</label>
                 <input type="radio" id="response" name="motor"/>
-                <label for="response">No Response 1</label>
+                <label for="response">No Response</label>
                 </div>
 </div>
             <button id="btn">Enter</button>
