9f26dce8cacc1cc7c0327dc422d4db424679b665
Tomzy.cs,Vb <oyemadetomiwa@gmail.com>
2024-01-17 17:24:06 +0100

    "updated on 17-1-24"

--- Frontend/receptionist/receptionist.js
+++ Frontend/receptionist/receptionist.js
@@ -361,35 +361,35 @@ function stopCamera() {
 
 ///APPOITMENT
 
-DOCTOR
-const doctorsData = {
-    cardiologist: ['Dr. Tomiwa', 'Dr. Johnson'],
-    dermatologist: ['Dr. Kingsley', 'Dr. White'],
-    surgeon:['Dr. Priceless', 'Dr John'],
-    psychiatrist:['Dr. Towa', 'Dr Paul'],
-    family_medicine:['Dr. Praise', 'Dr Trinity'],
-    dermatologist: ['Dr.Tom', 'Dr Ruth' ],
-    anaesthesiology:['Dr. Drake', 'Dr. Drake'],
-    rheumatologist:['Dr. Peace', 'Dr Jude'],
-    endocrinologist:['Dr. Grace', 'Dr. Houston'],
-    nephrologist:['Dr. Goodness', 'Dr Goodnews'],
-    neurologist:['Dr. Goodness', 'Dr.Peace'],
-    pediatrician:['Dr. Fooad', 'Dr. Fooad'],
-    urologist:['Dr. Uro', 'Dr. Fooad'],
-    radiologist:['Dr. Fooad', 'Dr. Fooad'],
-    dentist:['Dr. Gofade', 'Dr. Fooad'],
-    pulmonologist:['Dr. Foatt', 'Dr. Fooad'],
-    podiatristian:['Dr. Foatt', 'Dr. Fogad'],
-    emergency_physician:['Dr. Good', 'Dr. Tom'],
-    anaesthesiologist:['Dr. Green', 'Dr. Green'],
-    cardiologist:['Dr Ben', 'Dr. White'],
-    oncologist:['Dr. Bemson', 'Dr. Green'],
-    gastroenterologist:['Dr. Houston', 'Dr. Green'],
-    ophthanlmologist:['Dr. Jous', 'Dr. King'],
-    cardology:['Dr. Funke', 'Dr Roseline'],
-    allergist:['Dr. Postel', 'Dr.Houston'],
-    orthopedic_surgoen:['Dr.Lookman', 'Dr. Chelsea'],
-  };
+//DOCTOR
+// const doctorsData = {
+//     cardiologist: ['Dr. Tomiwa', 'Dr. Johnson'],
+//     dermatologist: ['Dr. Kingsley', 'Dr. White'],
+//     surgeon:['Dr. Priceless', 'Dr John'],
+//     psychiatrist:['Dr. Towa', 'Dr Paul'],
+//     family_medicine:['Dr. Praise', 'Dr Trinity'],
+//     dermatologist: ['Dr.Tom', 'Dr Ruth' ],
+//     anaesthesiology:['Dr. Drake', 'Dr. Drake'],
+//     rheumatologist:['Dr. Peace', 'Dr Jude'],
+//     endocrinologist:['Dr. Grace', 'Dr. Houston'],
+//     nephrologist:['Dr. Goodness', 'Dr Goodnews'],
+//     neurologist:['Dr. Goodness', 'Dr.Peace'],
+//     pediatrician:['Dr. Fooad', 'Dr. Fooad'],
+//     urologist:['Dr. Uro', 'Dr. Fooad'],
+//     radiologist:['Dr. Fooad', 'Dr. Fooad'],
+//     dentist:['Dr. Gofade', 'Dr. Fooad'],
+//     pulmonologist:['Dr. Foatt', 'Dr. Fooad'],
+//     podiatristian:['Dr. Foatt', 'Dr. Fogad'],
+//     emergency_physician:['Dr. Good', 'Dr. Tom'],
+//     anaesthesiologist:['Dr. Green', 'Dr. Green'],
+//     cardiologist:['Dr Ben', 'Dr. White'],
+//     oncologist:['Dr. Bemson', 'Dr. Green'],
+//     gastroenterologist:['Dr. Houston', 'Dr. Green'],
+//     ophthanlmologist:['Dr. Jous', 'Dr. King'],
+//     cardology:['Dr. Funke', 'Dr Roseline'],
+//     allergist:['Dr. Postel', 'Dr.Houston'],
+//     orthopedic_surgoen:['Dr.Lookman', 'Dr. Chelsea'],
+//   };
   
   function getDoctors() {
     const selectedRole = document.getElementById('roles').value;
