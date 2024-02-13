ba2676f0c496ed9ea63359c5e025b72b59d868a3
Tomzy.cs,Vb <oyemadetomiwa@gmail.com>
2024-02-13 12:45:30 +0100

    "updated on"13-2-24"

---
+++ doctor/meta.php
@@ -0,0 +1,23 @@
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="ROBOTS" content="ALL">
+<meta name="Engine" content="all">
+<meta name="distribution" content="global">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<!-------------------------------------------------------------------->
+<link href="<?php echo $website_url?>/Health_Records_and_Information/images/images/icon.png" rel="shortcut icon" type="image-png"/>
+
+<link href="<?php echo $website_url?>/doctor/css/icons-1.10.2/font/bootstrap-icons.css" type="text/css" rel="stylesheet" >
+<link href="<?php echo $website_url?>/Health_Records_and_Information/css/animate.css" type="text/css" rel="stylesheet" media="all">
+<link href="<?php echo $website_url?>/Health_Records_and_Information/css/aos.css" type="text/css" rel="stylesheet" />
+<link href="<?php echo $website_url?>/Health_Records_and_Information/css/dashboard.css" type="text/css" rel="stylesheet" />
+
+<meta property="og:type" content="Website" />
+<meta property="og:site_name" content="<?php echo $thename?>">
+<meta property="og:url" content="<?php echo $website_address?>" />
+
+<script src="<?php echo $website_url?>/Health_Records_and_Information/js/jquery-v3.6.1.min.js"></script>
+<script src="<?php echo $website_url?>/Health_Records_and_Information/js/aos.js"></script> 
+<script src="<?php echo $website_url?>/js/scripts.js"></script> 
+<script src="<?php echo $website_url?>/Health_Records_and_Information/js/receptionist.js"></script>
+
+
