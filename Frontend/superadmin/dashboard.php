f21773f939bbb8dd08c00e91672e966291f06160
Kingslet Effiong <kingsleyeffiong642@gmail.com>
2024-07-16 10:49:04 +0100

    udpayed

--- Frontend/superadmin/dashboard.php
+++ Frontend/superadmin/dashboard.php
@@ -38,10 +38,8 @@
 rel="stylesheet"
 href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"
 />
-<link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
-<!--Tomiwa font-->
+l
+<link href="awesome-font/css/font/bootstrap-icons.css" type="text/css" rel="stylesheet"/>
 <link href="../awesome-font/css/font-awesome.min.css" type="text/css" rel="stylesheet"/>
     <title>Hospital management System</title>
 
@@ -66,154 +64,79 @@ href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"
         }
     </script>
 </head>
-
 <body>
-<div class="navbar" > 
-    <div class="toggle" id="open-toggle">
-        <i class="fa fa-toggle-on"></i>
+<div class="navbar">
+        <div class="display__date">
+        </div>
+          <div class="profile">
+            <div class="profile_account hide">
+            <img id="image_profile_account" src="../Images/24b23c44ac34e5a0fb80978cd976604c.jpg" alt="">
+            <span>Super admin</span>
+            <button class="btn_submit">Upload Image</button>
+           </div>
 
-    </div>
-    <div class="div-logo">
-        <a href="#">Mount Tech Solution</a>
-    </div>
-    
-        <form method="post" action="../../backend/dashboardconfig/code.php" id="logoutform">
-            <input type="hidden" name="action" value="logout"/>    
-          
-            <button class="signIn"  onclick="document.getElementById('logoutform').submit();"><i class="fa fa-sign-out"></i></button>
-        </form>
-</div>
-<div class="contents">
-<nav class="sidebar" id="sidebar" >
-    <div class="profile_container">
-    <div class="profile"></div>
-        <span>DOCTOR SAM</span>
-        <span>ADMIN</span>
-    </div>
-    <div class="display_profile_container hide">
-        <div class="flexs">
-        <img src="../Images/doctor-offering-medical-teleconsultation.jpg" alt="">
-        <!-- <h1>DOCTOR SAM</h1> -->
-        <button class="profile_btn">UPLOAD PASSPORT</button>
-        <span style ="cursor:pointer;">Change password</span>
-        </div>
-    </div>
-        <ul class="menu-items">
-            <li class="items" onClick='openPatientsList()'>
-                <i class="fa fa-user" id="sidebar_icons"></i>
-              <span class="lists hide">Patients</span> 
-            </li>
-            <li class="items" onClick='openDoctorPage()'>
-            <i class="fa fa-user-md" id="sidebar_icons"></i>
-               <span class="lists hide">Doctor</span> 
-            </li>
-            <li class="items" onClick='openNursePage()'>
-                <i class="fa fa-user-md" id="sidebar_icons"></i>
-                <span class="lists hide">Nurse</span>
-            </li>
-            <li class="items"  onClick='openFinancePage()'>
-               <i class="fa fa-money" id="sidebar_icons"></i> 
-                <span class="lists hide">Finance</span>
-            </li>
-            <li class="items" onClick='openRecepPage()'>
-                <i class="fa fa-user-circle-o" id="sidebar_icons"></i>
-                <span class="lists hide">Receptionist</span>
-            </li>
-            <li class="items" onClick='openStaffPage()'>
-                <i class="fa fa-users" id="sidebar_icons"></i> 
-                <span class="lists hide">Staffs</span>
-            </li>
-            <li class="items" onClick='openWardBed()'>
-                <i class="fa fa-bed" id="sidebar_icons"></i>
-                <span class="lists hide">Wards & Beds</span>
-            </li>
-            <li class="items"onClick="show_profile_upload()">
-                <i class="fa fa-gear (alias)" id="sidebar_icons" ></i>
-                <span class="lists hide">Account</span>
-            </li>
-        </ul>
-</nav>
-<main>
-    <div class="container">
-        <h1>Dashboard</h1>
-        <div class="flex_wrap">
-        <div class="cards" onClick='openPatientsList()'>
-            <div class="card-icons">
-                <i class="fa fa-user"  id="card-icons"></i>
+            <div class="image">
+            <img src="../Images/24b23c44ac34e5a0fb80978cd976604c.jpg" alt="userImage"/>
+            <div class="active_on"></div>
             </div>
-                <h2>Patients</h2>
-        </div>
-        <div class="cards" onClick='openDoctorPage()'>
-            <div class="card-icons">
-                <i class="fa fa-user-md"  id="card-icons"></i>
-            </div>
-                <h2>Doctor</h2>
-        </div>
-        <div class="cards" onClick='openNursePage()'>
-            <div class="card-icons">
-                <i class="fa fa-user-md" id="card-icons"></i>
-            </div>
-                <h2>Nurse</h2>
-        </div>
-        <div class="cards" onClick='openPharmacyPage()'>
-            <div class="card-icons">
-                <i class="fa fa-plus-square "  id="card-icons"></i>
-            </div>
-                <h2>Phamacy</h2>
-        </div>
-        <div class="cards" onClick='openLabPage()'>
-            <div class="card-icons">
-                <i class="fa fa-flask" id="card-icons"></i>
-            </div>
-                <h2>Labouratory</h2>
-        </div>
-        <div class="cards" onClick='openPrecedurePage()'>
-            <div class="card-icons">
-                <i class="fa fa-hospital-o" id="card-icons"></i> 
-            </div>
-                <h2>Precedures</h2>
-        </div>
-        <div class="cards" onClick='openFinancePage()'>
-            <div class="card-icons">
-                <i class="fa fa-money " id="card-icons"></i> 
-            </div>
-                <h2>Finance</h2>
-        </div>
-        <div class="cards" onClick='openRecepPage()'>
-            <div class="card-icons">
-                <i class="fa fa-user-circle-o" id="card-icons"></i>
-            </div>
-                <h2>Receptionist</h2>
-        </div>
+            <span>Super admin</span>
+            <i class="bi bi-caret-down-fill" onclick="displayUserProfile()"></i>
+                 </div>
+                </div>
 
-        <div class="cards" onClick='openStaffPage()'>
-            <div class="card-icons">
-                <i class="fa fa-users" id="card-icons"></i> 
-            </div>
-                <h2>Staffs</h2>
-        </div>
-        <div class="cards" onClick='openWardBed()'>
-            <div class="card-icons">
-                <i class="fa fa-bed" id="card-icons"></i>
-            </div>
-                <h2>Wards & Beds</h2>
-                </div>
-        <div class="cards" onClick='openWardBed()'>
-            <div class="card-icons">
-                <i class="fa fa-stethoscope" id="card-icons"></i>
-            </div>
-                <h2>Matarnity</h2>
-                </div>
-        <div class="cards" onClick='openWardBed()'>
-            <div class="card-icons">
-                <i class="fa fa-users" id="card-icons"></i>
-            </div>
-                <h2>Staff Management</h2>
-                </div>
+    <div class="sidebar">
+    <div class="sidebar-body">
+      <ul>
+        <li class="links active">
+          <span>Patients tab</span>
+        </li>
+        <li class="links">
+          <span>Doctor tab</span>
+        </li>
+        <li class="links">
+          <span>Nurse tab</span>
+        </li>
+        <li class="links">
+          <span>Health record tab</span>
+        </li>
+        <li class="links">
+          <span>Labouratory tab</span>
+        </li>
+        <li class="links">
+          <span>Radiology tab</span>
+        </li>
+        <li class="links">
+          <span>Matanity tab</span>
+        </li>
+        <li class="links">
+          <span>Account tab</span>
+        </li>
+        <li class="links">
+          <span>Pharmacy tab</span>
+        </li>
+        <li class="links">
+          <span>Emergency tab</span>
+        </li>
+        <li class="links">
+          <span>Staff manager tab</span>
+        </li>
+        <li class="links">
+          <span>Surgical suite tab</span>
+        </li>
+        <li class="links">
+          <span>Morgue tab</span>
+        </li>
+        <li onclick="document.getElementById('logoutform').submit();" id="logout_link" class="links">
+          <span>Logout</span>
+          <form method="post" action="../../backend/dashboardconfig/code.php" id="logoutform">
+            <input type="hidden" name="action" value="logout"/>
+          </form>
+        </li>
+      </ul>
     </div>
-    </div>
-</main>
-</div>
+  </div>
+
+
     <script src="js/dashboard.js"></script>
 </body>
 </html>
--- Frontend/superadmin/js/dashboard.js
+++ Frontend/superadmin/js/dashboard.js
@@ -1,167 +1,34 @@
+function displayUserProfile(){
+    document.querySelector(".profile_account").classList.toggle("hide");
+};
+//////////////////////////////////////////
 
-//create a var for the open and the close toggle
-const openToggleSidebar = document.getElementById("open-toggle");
-const closeToggleSidebar = document.getElementById("close-toggle");
+const sections = document.querySelectorAll('.section');
+const allProfiles = document.querySelectorAll('.allProfiles');
+const links = document.querySelectorAll('.sidebar-body ul li');
 
+function toggleSidebarLinks(clickedLink){
+    links.forEach(link => link.classList.remove('active'));
+    clickedLink.classList.add('active');
+ }
+links.forEach(link => {
+    link.addEventListener('click', function() {
+        toggleSidebarLinks(this);
+    });
+});
+/////////////////////////////////////////////////////
 
-
-//making the sidebar to be invisible
-// sidebar.style.left = "fit-content"
-
-
-//this helps the toggle the sidebar open
-const sidebar_icons =()=>{
-    const sidebarIcon = document.querySelectorAll("#sidebar_icons")
-    for(let i =0; i < sidebarIcon.length; i++) {
-      sidebarIcon[i].style.fontSize = "20px"
-    }
+function createDate(){
+const now = new Date();
+const options = {
+day:'numeric',
+month:'numeric',
+year:'numeric',
+hour:'numeric',
+minute:'numeric',
+second:'numeric',
 }
-sidebar_icons()
-
-openToggleSidebar.addEventListener("click", function(){
-    const sidebar = document.querySelector(".sidebar");
-  sidebar.classList.toggle("active");
-    if(sidebar.classList.contains("active")){
-        const spanText = document.querySelectorAll(".lists")
-        for(let n =0; n<spanText.length; n++)
-        spanText[n].classList.remove("hide")
-
-    const sidebarIcon = document.querySelectorAll("#sidebar_icons")
-    for(let i =0; i < sidebarIcon.length; i++) {
-      sidebarIcon[i].style.fontSize = "20px"
-    }
-    const list = document.querySelectorAll(".items");
-    for(let l=0; l < list.length; l++){
-     list[l].addEventListener("mouseover",()=>{
-       list[l].style.color = " rgb(0, 158, 251)";
-      })
-      list[l].addEventListener("mouseout",()=>{
-        list[l].style.color = "#fff";
-      })
-    }
-    }
-    
-    else{
-        const spanText = document.querySelectorAll(".lists")
-        for(let n =0; n<spanText.length; n++)
-    spanText[n].classList.add("hide")
-
-    const sidebarIcon = document.querySelectorAll("#sidebar_icons")
-    for(let i =0; i < sidebarIcon.length; i++) {
-        sidebarIcon[i].style.fontSize = "20px"
-    }
-    const list = document.querySelectorAll(".items");
-    for(let l=0; l < list.length; l++){
-     list[l].addEventListener("mouseover",()=>{
-       list[l].style.color = "rgb(42, 87, 215)";
-      })
-      list[l].addEventListener("mouseout",()=>{
-        list[l].style.color = "#fff";
-      })
-    }
-    }
-    const profile_container = document.querySelector(".display_profile_container");
-    profile_container.classList.toggle("change_color");
-
-})
-
-
-const show_profile_upload = ()=>{
-    const profile_container = document.querySelector(".display_profile_container");
-    profile_container.classList.toggle("hide")
+const dateTime = new Intl.DateTimeFormat(navigator.language, options).format(now)
+document.querySelector('.display__date').textContent = dateTime
 }
-
-
-//patient page redirect
-
-function openPatientsList(){
-    let urlPatient = '../superadmin/pages/patients.php';
-    window.parent(location=(urlPatient));
-};
-function openDoctorPage(){
-    let urlDoctor = '/';
-    window.parent(location=(urlDoctor));
-};
-function openNursePage(){
-    let urlNurse = '/';
-    window.parent(location=(urlNurse));
-};
-function openPharmacyPage(){
-    let urlPharmacy = '/';
-    window.parent(location=(urlPharmacy));
-};
-function openLabPage(){
-    let urlLab = '/';
-    window.parent(location=(urlLab));
-};
-function openPrecedurePage(){
-    let urlPrecedure = '/';
-    window.parent(location=(urlPrecedure));
-};
-function openFinancePage(){
-    let urlFinance = '/';
-    window.parent(location=(urlFinance));
-}
-function openRecepPage(){
-    let urlRecep = '/';
-    window.parent(location=(urlRecep));
-};
-function openPhysiotherapyPage(){
-    let urlPhysiotherapy = '/';
-    window.parent(location=(urlPhysiotherapy));
-};
-function openStaffPage(){
-    let urlStaff = '/';
-    window.parent(location=(urlStaff));
-};
-function openWardBed(){
-    let urlWardBed = '/';
-    window.parent(location=(urlWardBed));
-};
-
-
-
-
-
-
-
-
-
-
-
-
-
-// <?php include '../../backend/config/connection.php';?>
-// <?php include '../../backend/dashboardconfig/session-validation.php';?>
-// <?php include '../../backend/config/user-validation.php';?>
-
-
-
-// <?php
-//  $staff_id= $_POST['staff_id'];
-// ?>
-
-// <?php    
-//     $fetch_staff_profile=$callclass->_get_user_details($conn, $s_staff_id);
-//     $staff_profile_array = json_decode($fetch_staff_profile, true);
-//     $fullname= $staff_profile_array[0]['fullname'];
-//     $email= $staff_profile_array[0]['email'];
-//     $phonenumber= $staff_profile_array[0]['phonenumber'];
-//    // $role_id= $staff_profile_array[0]['role_id'];
-//     $status_id= $staff_profile_array[0]['status_id'];
-//     $date= $staff_profile_array[0]['date'];
-//     $last_login= $staff_profile_array[0]['last_login'];
-//    $passport = $staff_profile_array[0]["passport"];
-    
-        
-//     $fetch_status=$callclass->_get_status_details($conn, $status_id);
-//     $status_array = json_decode($fetch_status, true);
-//     $status_name= $status_array[0]['status_name'];
-   
-// ?>
-
-
-
-
-
-
+setInterval(() => createDate());
