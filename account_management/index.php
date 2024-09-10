50b3301bb98f6a6986602fd594a7edb3d3289f70
Kingslet Effiong <kingsleyeffiong642@gmail.com>
2024-09-10 14:37:35 +0100

    updated

--- account_management/index.php
+++ account_management/index.php
@@ -67,53 +67,54 @@ $page = "account_unit_dash"; // Assign the value "account_unit_dash" to the $pag
 
     <!--DIFFERENT SECTIONS--->
     <div class="navbar">
-        <div class="display__date">
+        <div class="display__date animatetext">
         </div>
           <div class="profile">
             <div class="profile_account hide">
             <img id="image_profile_account" src="<?php echo $website_url ?>/doctor/images/doctor2.jpg" alt="">
-            <span><?php echo $fullname ?></span>
+            <span class="animatetext"><?php echo $fullname ?></span>
            </div>
 
             <div class="image">
             <img src="<?php echo $website_url ?>/doctor/images/doctor2.jpg" alt="userImage"/>
             </div>
-            <span><?php echo $fullname ?></span>
+            <span class="animatetext"><?php echo $fullname ?></span>
             <i class="bi bi-caret-down-fill" onclick="displayUserProfile()"></i>
                  </div>
                 </div>
 
     <div class="sidebar">
             <div class="sidebar-body">
-                <ul>
-                    <li onclick="pendingTransaction()" id="links" class="active scale-up-center">
-                        <i class="bi bi-graph-up"></i>
-                        <span>Pending transaction</span>
-                    </li>
-                    <li onclick="successfulTransaction()" id="links">
-                        <i class="bi bi-credit-card"></i>
-                        <span>Successful transactions</span>
-                    </li>
-                    <li onclick="overallTransactions()" id="links">
-                        <i class="bi bi-credit-card"></i>
-                        <span>Overall transactions</span>
-                    </li>
-                    <li onclick="document.getElementById('logoutform').submit();" id="links">
-                        <i class="fa-solid fa-right-from-bracket"></i>
-                        <span>Logout</span>
-                        <form method="post" action="../config/code.php" id="logoutform">
-                    <input type="hidden" name="action" value="logout"/>    
-                </form>
-                    </li>
-                </ul>
+            <ul>
+  <li onclick="pendingTransaction()" class="active scaleup" id="links">
+    <i class="bi bi-graph-up"></i>
+    <span>Pending transaction</span>
+  </li>
+  <li onclick="successfulTransaction()" class="scaleup" id="links">
+    <i class="bi bi-credit-card"></i>
+    <span>Successful transactions</span>
+  </li>
+  <li onclick="overallTransactions()" class="scaleup" id="links">
+    <i class="bi bi-credit-card"></i>
+    <span>Overall transactions</span>
+  </li>
+  <li onclick="document.getElementById('logoutform').submit();" class="scaleup" id="links">
+    <i class="fa-solid fa-right-from-bracket"></i>
+    <span>Logout</span>
+    <form method="post" action="../config/code.php" id="logoutform">
+      <input type="hidden" name="action" value="logout" />
+    </form>
+  </li>
+</ul>
+
             </div>
             <div class="sidebar-footer">
                     <div>
                     <i class="bi bi-person"></i>
-                    <span>USER ID: <?php echo $s_account_unit_id?></span>
+                    <span class="animatetext">USER ID: <?php echo $s_account_unit_id?></span>
                     </div>
-                    <div id="pos">POS Amount: 0.00</div>
-                    <div id="cash">Cash Amount: 0.00</div>
+                    <div class="animatetext" id="pos">POS Amount: 0.00</div>
+                    <div class="animatetext" id="cash">Cash Amount: 0.00</div>
                 </div>
             </div>
         </div>
@@ -122,14 +123,14 @@ $page = "account_unit_dash"; // Assign the value "account_unit_dash" to the $pag
         <div class="pending__transactions">
             <div class="patient_list_div">
             <div class="search_bar_container">
-                <h3>Pending Transactions</h3>
-                    <input type="text" placeholder="Search here" id="psearch">
+                <h3 class="animatetext">Pending Transactions</h3>
+                    <input type="text" placeholder="Search here" id="psearch" class="scaleup">
                 </div>
 
 
                     <table id="pending">
                         <thead>
-                            <tr>
+                            <tr class="animatetext">
                                 <td>S/N</td>
                                 <td>PASSPORT</td>
                                 <td>Patient Id</td>
@@ -150,7 +151,7 @@ $page = "account_unit_dash"; // Assign the value "account_unit_dash" to the $pag
             </div>
             </div>
         </div>
-    
+
 
         <div class="modal hidden" id="patient__receipt">
         <button class="btn--close-modal" onclick="closeModal('patient__receipt')">&times;</button>
@@ -177,19 +178,19 @@ $page = "account_unit_dash"; // Assign the value "account_unit_dash" to the $pag
             </tbody>
         </table>
         <h3 id="total__amount">Total: ₦3,000</h3>
-        <button onclick="printInvoice()">Print</button>
+        <button onclick="printInvoice()" class="bg-blue">Print</button>
     </div>
 
         <div class="successful__transactions hide">
         <div class="patient_list_div">
             <div class="search_bar_container">
                 <input type="hidden" id="account_id" value="<?php echo $s_account_unit_id ?>"/>
-                <h3>Successful Transactions</h3>
-                    <input type="text" placeholder="Search here" id="ssearch">
+                <h3 class="animatetext">Successful Transactions</h3>
+                    <input type="text" placeholder="Search here" id="ssearch" class="scaleup">
                 </div>
                   <table id="success">
                       <thead>
-                          <tr>
+                          <tr class="animatetext">
                               <td>S/N</td>
                               <td>PASSPORT</td>
                               <td>Patient Name</td>
@@ -219,12 +220,12 @@ $page = "account_unit_dash"; // Assign the value "account_unit_dash" to the $pag
         <div class="overall__transactions hide">
         <div class="patient_list_div">
             <div class="search_bar_container">
-                <h3>Overall transactions</h3>
-                    <input type="text" placeholder="Search here" id="osearch">
+                <h3 class="animatetext">Overall transactions</h3>
+                    <input type="text" placeholder="Search here" id="osearch" class="scaleup">
                 </div>
-                    <table id="overall__transactions">
+                    <table id="overall__transactions" class="tilt-in-fwd-tr">
                                             <thead>
-                                                <tr>
+                                                <tr class="animatetext">
                                                     <td>S/N</td>
                                                     <td>Passport</td>
                                                     <td>Patient Id</td>
@@ -243,12 +244,25 @@ $page = "account_unit_dash"; // Assign the value "account_unit_dash" to the $pag
                         </table>
                     </div>
                     <div class="overall_amount">
-                <h3 id="overall__amounts"></h3>
+                <h3 id="overall__amounts" class="animatetext"></h3>
             </div>
                 </div>
             </div>
             </div>
         </div>
         <div class="black--background hidden"></div>
+        <script>
+            let links =  document.querySelectorAll('#links');
+            function toggleSidebarLinks(clickedLink){
+                links.forEach(link => link.classList.remove('active'));
+                clickedLink.classList.add('active');
+            }
+            links.forEach(link => {
+                link.addEventListener('click', function() {
+                    toggleSidebarLinks(this);
+                });
+            });
+
+        </script>
 </body>
 </html>
